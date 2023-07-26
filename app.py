e2a613e20f87600ab8ace167750c23c6a614e3f6
andy19910102 <andy19910102@gmail.com>
2023-07-26 21:36:49 +0800

    feature: add sticker and location handler

--- app.py
+++ app.py
@@ -14,11 +14,16 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
+    StickerMessage,
+    LocationMessage,
 )
+
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent
+    TextMessageContent,
+    StickerMessageContent,
+    LocationMessageContent,
 )
 
 app = Flask(__name__)
@@ -30,27 +35,75 @@ handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
+    print("#" * 40)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+    print("#" * 40)
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
+        # 當使用者傳入文字訊息時
         line_bot_api = MessagingApi(api_client)
+        user_msg = event.message.text
+        bot_msg = TextMessage(text=f"What you said is: {user_msg}")
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[
+                    bot_msg
+                ]
+            )
+        )
+
+@handler.add(MessageEvent, message=StickerMessageContent)
+def handle_sticker_message(event):
+    with ApiClient(configuration) as api_client:
+        # 當使用者傳入貼圖時
+        line_bot_api = MessagingApi(api_client)
+        sticker_id = event.message.sticker_id
+        package_id = event.message.package_id
+        keywords = ", ".join(event.message.keywords)
+        # 可以使用的貼圖清單
+        # https://developers.line.biz/en/docs/messaging-api/sticker-list/
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    StickerMessage(package_id="6325", sticker_id="10979904"),
+                    TextMessage(text=f"You just sent a sticker. Here is the information of the sticker:"),
+                    TextMessage(text=f"package_id is {package_id}, sticker_id is {sticker_id}."),
+                    TextMessage(text=f"The keywords are {keywords}."),
+                ]
+            )
+        )
+
+@handler.add(MessageEvent, message=LocationMessageContent)
+def handle_location_message(event):
+    with ApiClient(configuration) as api_client:
+        # 當使用者傳入地理位置時
+        line_bot_api = MessagingApi(api_client)
+        latitude = event.message.latitude
+        longitude = event.message.longitude
+        address = event.message.address
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    TextMessage(text=f"You just sent a location message."),
+                    TextMessage(text=f"The latitude is {latitude}."),
+                    TextMessage(text=f"The longitude is {longitude}."),
+                    TextMessage(text=f"The address is {address}."),
+                    LocationMessage(title="Here is the location you sent.", address=address, latitude=latitude, longitude=longitude)
+                ]
             )
         )
 
